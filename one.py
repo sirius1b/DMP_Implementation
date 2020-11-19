f95809a6f0c9349f1f7f557c6c93217669feee92
broompa <lavanya18155@iiitd.ac.in>
2020-11-19 12:23:04 +0530

    matlab script

--- one.py
+++ one.py
@@ -22,45 +22,46 @@ def get_gaussians(s,c,h,n,f_s): #equally spaced gaussian basis
 	# linear least squares
 	for i in range(len(s)):
 		K[i,:] = K[i,:]*s[i]/sum(K[i,:])
-	wi_lq = least_sq(K,f_s)
-	f_new = np.matmul(K,wi_lq)
+	# wi_lq = least_sq(K,f_s)
+	# f_new = np.matmul(K,wi_lq)
 	
 	#lWR
 	S = s
 	wi_lw = np.ones(n)
 	for i in range(n):
-		qsi_mat = np.diag(K1[:,i])
+		qsi_mat = np.diag(K[:,i])
 		# print(qsi_mat.shape,S.shape,f_s.shape)
 		wi_lw[i] = np.matmul(np.matmul(S.T,qsi_mat),f_s)/np.matmul(np.matmul(S.T,qsi_mat),S)
 
-	
-	print(wi_lw)
-	print(wi_lq)
+	# print(wi_lw)
+	# print(wi_lq)
 	f_lwr = np.matmul(K,wi_lw)
 	plt.subplot(2,2,2)
 	# plt.plot(s,f_new,label='f_new')
 	plt.plot(s,f_s,label='f_s')
 	plt.plot(s,f_lwr,label='f_lwr')
 	plt.legend()
-	# plt.show()
+	plt.show()
 
 
 def dmp(x,xd,xdd,t0,tf,td,t):
-	tau = tf - t0;
+	tau = 1;
 	v = xd*tau
 	vd = xdd*tau
 	K = 1; D = np.sqrt(4*K);
-	g = y[len(y) - 1];
-	x0 = y[0];
+	g = x[len(x) - 1];
+	x0 = x[0];
 	f_s = (-K*(g - x) + D*v + tau*vd)/(g-x0)
 
 	alp = 1
 	s = np.exp(-alp*t/tau)
 
-	ft = get_gaussians(s,1,1000,50,f_s)
+	ft = get_gaussians(s,1,500,50,f_s)
+
+	#---------------------
+
 
 	#-------------------
-
 
 	# plt.subplot(2,2,1)
 	# plt.plot(t,x)
@@ -79,7 +80,6 @@ def dmp(x,xd,xdd,t0,tf,td,t):
 
 	# plt.legend()
 
-	plt.show()
 
 
 
@@ -95,10 +95,10 @@ if __name__ == '__main__':
 	## some parameters
 	t0 = 1 ; tf = 10; td = 0.05;
 	t = np.arange(t0,tf,td)
-	# y = list(map(lambda x: 1 if x>2.5 else 0 , t))
-	a = 3;
-	y = np.array(7.2*t**2 + 16*t -0.43)
-	# y = np.sin(a*t)
+	# y = np.array(list(map(lambda x: 1 if x>2.5 else 0 , t)))
+	a = 0.1;
+	# y = np.array(7.2*t**2 + 16*t -0.43)
+	y = np.sin(a*t)
 	yd = derivate_bk(y,td)
 	ydd = derivate_bk(yd,td)
 
